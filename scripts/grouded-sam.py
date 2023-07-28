2fbec980e9c61835cddd3966882316ea98e430e4
hai hui <40460789+iuhiah@users.noreply.github.com>
2023-07-28 17:05:48 +0800

    update script

--- scripts/grouded-sam.py
+++ scripts/grouded-sam.py
@@ -1,24 +1,23 @@
+# visualisation
 import numpy as np
 import torch
 from PIL import Image
 
-# Grounding DINO
+# GroundingDINO
 from groundingdino.util import box_ops
 from groundingdino.util.inference import annotate, load_image, predict, load_model
 
-# segment anything
+# SAM
 from segment_anything import build_sam, SamPredictor 
 import cv2
 import numpy as np
 import torch
 
-# load groundingdino
-def loadGroundingDINO(ckpt_config, ckpt_file):
+# load models
+def loadDINO(ckpt_config, ckpt_file):
     model = load_model(ckpt_config, ckpt_file)
     print("GroundingDINO loaded.")
     return model
-
-# load SAM
 def loadSAM(sam_ckpt, device):
     sam = build_sam(checkpoint=sam_ckpt)
     sam.to(device=device)
@@ -26,8 +25,22 @@ def loadSAM(sam_ckpt, device):
     print("SAM loaded.")
     return sam_predictor
 
-# run groundingdino
-def runGroundingDINO(prompt, image_path, model, device):
+# SAM helper
+def show_mask(mask, image, random_color=True):
+    if random_color:
+        color = np.concatenate([np.random.random(3), np.array([0.8])], axis=0)
+    else:
+        color = np.array([30/255, 144/255, 255/255, 0.6])
+    h, w = mask.shape[-2:]
+    mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
+    
+    annotated_frame_pil = Image.fromarray(image).convert("RGBA")
+    mask_image_pil = Image.fromarray((mask_image.cpu().numpy() * 255).astype(np.uint8)).convert("RGBA")
+
+    return np.array(Image.alpha_composite(annotated_frame_pil, mask_image_pil))
+
+# run models
+def runDINO(prompt, image_path, model, output_path, device):
     TEXT_PROMPT = prompt
     BOX_TRESHOLD = 0.3
     TEXT_TRESHOLD = 0.25
@@ -49,26 +62,13 @@ def runGroundingDINO(prompt, image_path, model, device):
         return None, None, None
     else:
         annotated_frame = annotate(image_source=image_source, boxes=boxes, logits=logits, phrases=phrases)
-        # cv2.imwrite(output_path, annotated_frame)
-        # annotated_frame = annotated_frame[...,::-1]
+
+        if output_path == None: pass
+        else:
+            cv2.imwrite(output_path, annotated_frame)
+            annotated_frame = annotated_frame[...,::-1]
 
         return image_source, boxes, annotated_frame
-
-# sam mask
-def show_mask(mask, image, random_color=True):
-    if random_color:
-        color = np.concatenate([np.random.random(3), np.array([0.8])], axis=0)
-    else:
-        color = np.array([30/255, 144/255, 255/255, 0.6])
-    h, w = mask.shape[-2:]
-    mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
-    
-    annotated_frame_pil = Image.fromarray(image).convert("RGBA")
-    mask_image_pil = Image.fromarray((mask_image.cpu().numpy() * 255).astype(np.uint8)).convert("RGBA")
-
-    return np.array(Image.alpha_composite(annotated_frame_pil, mask_image_pil))
-
-# run sam
 def runSAM(sam_predictor, image_source, boxes, annotated_frame, output_path, device):
     # set image
     sam_predictor.set_image(image_source)
@@ -89,28 +89,25 @@ def runSAM(sam_predictor, image_source, boxes, annotated_frame, output_path, dev
         annotated_frame = show_mask(masks[i][0], annotated_frame)
     cv2.imwrite(output_path, annotated_frame)
 
-### MAIN PROGRAM
+### MAIN PROGRAM (run from project root)
 if __name__ == "__main__":
-    with open(r"scripts/arguments.txt") as f:
-        args = f.read().splitlines()
+    filename= "0009"
+    prompt = "carpark . car park" ### change for audio input
 
-    filename = args[0]
-    # prompt = speechToText("images/" + filename + ".mp3") if args[1] == "from audio" else args[1]
-    prompt = args[1]
-
-    device = "cpu"
+    device = "cpu" # or CUDA
 
     # CHECKPOINT PATHS
     gd_ckpt_config = "groundingdino/groundingdino/config/GroundingDINO_SwinT_OGC.py"
     gd_ckpt_file = "groundingdino/weights/groundingdino_swint_ogc.pth"
+    sam_ckpt = "segment-anything/checkpoints/sam_vit_h_4b8939.pth"
 
-    sam_ckpt = "SAM/checkpoints/sam_vit_h_4b8939.pth"
-
+    # IMAGE PATHS
     image_path = "images/" + filename + ".png"
-    sam_output_path = "images/" + filename + "-mask.jpg"
+    dino_output_path = None # change if you want to get bounding box output
+    sam_output_path = "images/" + filename + "-mask.png"
 
-    groundingDINO_model = loadGroundingDINO(ckpt_config=gd_ckpt_config, ckpt_file=gd_ckpt_file)
-    image_source, boxes, annotated_frame = runGroundingDINO(prompt, image_path, groundingDINO_model, device)
+    dino_model = loadDINO(ckpt_config=gd_ckpt_config, ckpt_file=gd_ckpt_file)
+    image_source, boxes, annotated_frame = runDINO(prompt, image_path, dino_model, dino_output_path, device)
 
     if boxes != None:
         sam_predictor = loadSAM(sam_ckpt, device)
