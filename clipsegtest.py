7892c59e20ee366cd0c60e7b06eb9ebc508771c5
iuhiah <leehaihui2004@gmail.com>
2023-08-24 03:15:50 +0800

    add readme, rename files

--- clipsegtest.py
+++
@@ -1,5 +0,0 @@
-with open(r"arguments.txt") as f:
-    text = f.read().splitlines()
-    prompts = [i.strip() for i in text[1].split(",")]
-
-    print(text)
--- transcribe.py
+++
@@ -1,5 +0,0 @@
-import whisper
-
-model = whisper.load_model("base.en")
-result = model.transcribe("") # path to audio file
-print(result["text"])
